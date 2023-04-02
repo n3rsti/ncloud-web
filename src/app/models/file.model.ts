51ece6838b966cdb32d12a51cd92e838261fcd12
n3rsti <n3rsti@protonmail.com>
2023-04-02 12:16:46 +0200

    add human readable size to file-details instead of raw size in bytes

--- src/app/models/file.model.ts
+++ src/app/models/file.model.ts
@@ -89,6 +89,30 @@ export class FileModel {
     return this._size;
   }
 
+  get humanReadableSize(): string {
+    let bytes = this.size;
+    let dp = 1;
+
+
+    const thresh = 1024;
+
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+
+    const units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    let u = -1;
+    const r = 10**dp;
+
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+    return bytes.toFixed(dp) + ' ' + units[u];
+  }
+
   set size(value: number) {
     this._size = value;
   }
